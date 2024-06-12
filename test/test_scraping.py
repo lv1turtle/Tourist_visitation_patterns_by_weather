223f51990a4d62b198b8199111c57dbaa79ee853
kdk0411 <wnsldjqja@gmail.com>
2024-06-12 19:43:29 +0900

    feat: Add Tourism-API Test Code

---
+++ test/test_scraping.py
@@ -0,0 +1,97 @@
+import pytest
+import pandas as pd
+import requests
+import os
+
+from airflow.models import DagBag, XCom
+from airflow.utils import dates
+
+from unittest.mock import patch, MagicMock
+from dags.scraping-tourism-API import get_csv_from_s3, get_data_from_API, concat_data, save_csv_to_s3
+
+
+dag_bag = DagBag(dag_folder='dags/', include_examples=False)
+dag = dag_bag.get_dag('scraping_tourism_API')
+
+# DAG 객체 확인
+def test_dag_loaded():
+    assert dag is not None
+    assert dag.dag_id == 'scraping_tourism_API'
+    
+# Task명 일치여부
+def test_tasks_exist():
+    tasks = dag.tasks
+    task_ids = [task.task_id for task in tasks]
+    expected_tasks = ['get_csv_from_s3', 'get_data_from_API', 'concat_data', 'save_csv_to_s3']
+    assert set(task_ids) == set(expected_tasks)
+    
+# Task의 의존성 테스트
+def test_task_dependency():
+    tasks = dag.tasks
+    get_csv_task = [task for task in tasks if task.task_id == 'get_csv_from_s3'][0]
+    get_data_task = [task for task in tasks if task.task_id == 'get_data_from_API'][0]
+    concat_data_task = [task for task in tasks if task.task_id == 'concat_data'][0]
+    save_csv_task = [task for task in tasks if task.task_id == 'save_csv_to_s3'][0]
+    
+    assert get_csv_task.downstream_task_ids == {'concat_data'}
+    assert get_data_task.downstream_task_ids == {'concat_data'}
+    assert concat_data_task.downstream_task_ids == {'save_csv_to_s3'}
+    assert save_csv_task.downstream_task_ids == set()
+    
+# get_csv_from_s3 Function Logic Test
+@patch('dags.scraping-API.S3Hook')
+def test_get_csv_from_s3(mock_s3_hook):
+    mock_response = MagicMock()
+    csv_data = (
+        "signguCode,signguNm,daywkDivNm,touDivNm,touNum,baseYmd",
+        "1111000000,종로구,금요일,현지인(a),71358.00,20210101",
+        "1111000000,종로구,금요일,외지인(b),122265.50,20210101",
+        "1111000000,종로구,금요일,외국인(c),26.46,20210101",
+        "1114000000,중구,금요일,현지인(a),58156.00,20210101",
+        "1114000000,중구,금요일,외지인(b),107786.00,20210101"
+    )
+    mock_response.decode.return_value = csv_data
+    mock_s3_hook.return_value.read_key_value = mock_response
+    
+    kwargs = {'params': {'bucket_name':'test-bucket', 'key':'test.csv'}, 'ti':MagicMock()}
+    get_csv_from_s3(**kwargs)
+    
+    expected_df = pd.DataFrame({
+        'signguCode':[1111000000, 1111000000, 1111000000, 1114000000, 1114000000],
+        'signguNm':['종로구', '종로구', '종로구', '중구', '중구'],
+        'daywkDivNm':['금요일', '금요일', '금요일', '금요일', '금요일'],
+        'touDivNm':['현지인(a)', '외지인(b)','외국인(c)', '현지인(a)', '외지인(b)'],
+        'touNum':[71358.00, 122265.50, 26.46, 58156.00, 107786.00],
+        'baseYmd':[20210101, 20210101, 20210101, 20210101, 20210101]
+    })
+    
+    xcom_data = XCom.get_one(key='csv_data', task_id='get_csv_from_s3')
+    result_df= pd.read_json(xcom_data)
+    # 결과 Dataframe과 예상 Dataframe 비교
+    assert result_df.equals(expected_df)
+    
+# API Load Test
+def test_get_data_from_API():
+    service_key = os.getenv('AIRFLOW_TOURISM_API_KEY')
+    assert service_key is not None,"API Key does not exist in the environment variables."
+    
+    url = "http://apis.data.go.kr/B551011/DataLabService/locgoRegnVisitrDDList"
+    params = {
+        "serviceKey": service_key,
+        "numOfRows": 1,
+        "pageNo": 1,
+        "MobileOS": "ETC",
+        "MobileApp": "TestApp",
+        "startYmd": 20240101,
+        "endYmd": 20240101,
+        "_type": "json"
+    }
+    
+    response = requests.get(url, params=params)
+    
+    assert response.status_code == 200, "API request Failed"
+    
+    data = response.json()
+    assert 'response' in data, "Response Error"
+    assert 'body' in data['response'], "Body Error"
+    assert 'items' in data['response']['body'], "Item Error"
---
+++ test/test_slack.py
@@ -0,0 +1,31 @@
+import os
+import pytest
+from unittest.mock import patch
+from airflow.models import Variable
+from dags.plugins.slack import send_message_to_a_slack_channel
+
+# pytest.fixure를 사용하면 테스트 실행 전후에 특정 작업을 넣어줄 수 있다.
+@pytest.fixture
+def test_slack_webhook_url(monkeypatch):
+    # pytest의 monkeypatch plugin을 사용하면 환경변수를 일시적으로 변경해줄 수 있다.
+    slack_url = os.environ.get("AIRFLOW_SLACK_URL")
+    
+    if not slack_url:
+        pytest.skip("Black ERROR : Slack_Url does not exist in the environment variables.")
+    
+    
+    monkeypatch.setenv("AIRLFOW_SLACK_URL", slack_url)
+    
+    yield
+    monkeypatch.undo()
+    
+@patch('airflow.models.Variable.get')
+def test_send_message(mock_variable_get, test_slack_webhook_url):
+    slack_url = os.environ.get("AIRFLOW_SLACK_URL")
+    
+    mock_variable_get.return_value = slack_url
+    test_message = "Send a test message from airflow"
+    
+    response = send_message_to_a_slack_channel(test_message)
+    
+    assert response.status_code == 200, "Failed to send message to Slack."
