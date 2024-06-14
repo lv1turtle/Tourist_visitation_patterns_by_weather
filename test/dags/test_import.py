f28775556e3699dad91d9d29e90fe53ec8f784bb
kdk0411 <wnsldjqja@gmail.com>
2024-06-14 16:44:00 +0900

    feat: fix testcase

--- test/dags/test_import.py
+++ test/dags/test_import.py
@@ -2,4 +2,4 @@ from airflow.models import DagBag
 
 def test_import_error():
     dag_bag = DagBag()
-    assert len(dag_bag.import_error) == 0, "No Import Failures"
+    assert len(dag_bag.import_errors) == 0, "No Import Failures"
