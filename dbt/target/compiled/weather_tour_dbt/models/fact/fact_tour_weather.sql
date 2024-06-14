5f5d2c2a198a277534f0b8d67cb11018700d817a
lv1.turtle <skqltldnjf77@gmail.com>
2024-06-14 14:04:10 +0900

    Create dbt

---
+++ dbt/target/compiled/weather_tour_dbt/models/fact/fact_tour_weather.sql
@@ -0,0 +1,89 @@
+
+WITH  __dbt__cte__src_weather_api as (
+WITH src_weather_api AS (
+    SELECT * FROM wnsldjqja.weather_info
+)
+SELECT
+    spotName,
+    tm,
+    courseAreaId,
+    spotAreaName,
+    th3,
+    ws,
+    sky,
+    rhm,
+    pop
+FROM
+    src_weather_api
+),  __dbt__cte__src_signgu as (
+WITH src_signgu AS (
+    SELECT * FROM wnsldjqja.signgu
+)
+SELECT 
+    "시군구코드" signguCode, 
+    "시도명" cityNm,
+    "시군구명" signguNm
+FROM
+    src_signgu
+),  __dbt__cte__src_tour_api as (
+WITH src_tour_api AS (
+    SELECT * FROM wnsldjqja.tourism
+), src_signgu AS(
+    SELECT * FROM __dbt__cte__src_signgu
+)
+SELECT 
+    left(signguCode,5) signguCode,
+    baseYmd,
+    daywkDivNm,
+    touDivNm,
+    touNum
+FROM src_tour_api
+), src_weather_tot AS (
+    SELECT
+        a.spotName,
+        tm,
+        th3,
+        ws,
+        sky,
+        rhm,
+        pop,
+        b.signguCode,
+        b.signguNm,
+        b.cityNm
+    FROM __dbt__cte__src_weather_api a
+    LEFT JOIN "dev"."wnsldjqja"."dim_tourist_spot" b
+    ON a.spotName = b.spotName
+), src_tour_api AS (
+    SELECT
+        a.signguCode,
+        b.cityNm,
+        b.signguNm,
+        baseYmd,
+        daywkDivNm,
+        touDivNm,
+        touNum
+    FROM __dbt__cte__src_tour_api a
+    JOIN __dbt__cte__src_signgu b
+    ON a.signguCode = b.signguCode
+), tmp AS(
+    SELECT
+        baseYmd,
+        date_part('hour',tm) tm_hour,
+        spotName,
+        th3,
+        ws,
+        sky,
+        rhm,
+        pop,
+        daywkDivNm,
+        touNum,
+        touDivNm
+    FROM src_weather_tot a
+    JOIN src_tour_api b
+    ON CONCAT(a.cityNm,a.signguNm) = CONCAT(b.cityNm,b.signguNm)
+    AND TO_CHAR(a.tm,'YYYYMMDD') = TO_CHAR(b.baseYmd,'YYYYMMDD')
+)
+SELECT * FROM tmp
+WHERE baseYmd is not NULL
+
+    AND TO_CHAR(baseYmd,'YYYYMMDD') not in (SELECT DISTINCT TO_CHAR(baseYmd,'YYYYMMDD') from "dev"."wnsldjqja"."fact_tour_weather")
---
+++ dbt/target/run/weather_tour_dbt/models/dim/dim_tourist_spot.sql
@@ -0,0 +1,75 @@
+
+  
+    
+
+  create  table
+    "dev"."wnsldjqja"."dim_tourist_spot__dbt_tmp"
+    
+    
+    
+  as (
+    WITH  __dbt__cte__src_signgu as (
+WITH src_signgu AS (
+    SELECT * FROM wnsldjqja.signgu
+)
+SELECT 
+    "시군구코드" signguCode, 
+    "시도명" cityNm,
+    "시군구명" signguNm
+FROM
+    src_signgu
+),  __dbt__cte__src_tourist_spot as (
+WITH src_tourist_spot AS (
+    SELECT * FROM wnsldjqja.tourist_spot 
+)
+SELECT 
+    "관광지명" spotName,
+    "시/군/구 코드" signguCode,
+    "테마명" thema
+FROM
+    src_tourist_spot
+),  __dbt__cte__src_korea_dept as (
+WITH src_korea_dept AS (
+    SELECT * FROM wnsldjqja.korea_dept
+)
+SELECT 
+    "지역코드" deptId, 
+    "지역이름" cityNm
+FROM
+    src_korea_dept
+), sg AS (
+    SELECT
+        signguCode,
+        cityNm,
+        signguNm
+    FROM __dbt__cte__src_signgu
+), ts AS(
+    SELECT 
+        spotName,
+        signguCode,
+        thema
+    FROM __dbt__cte__src_tourist_spot
+), kd AS(
+    SELECT * FROM __dbt__cte__src_korea_dept
+), tmp AS(
+SELECT
+    ts.spotName,
+    ts.thema,
+    sg.signguCode,
+    sg.cityNm,
+    sg.signguNm
+FROM ts
+LEFT JOIN sg ON ts.signguCode = sg.signguCode
+)
+
+SELECT
+    spotName,
+    thema,
+    signguCode,
+    tmp.cityNm,
+    signguNm,
+    deptId
+FROM tmp
+LEFT JOIN kd ON tmp.cityNm = kd.cityNm
+  );
+  
